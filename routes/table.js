b9d03f7f54090b6f5d579da2a670218e8b7c2218
GUNDAANUTEJ <77635770+GUNDAANUTEJ@users.noreply.github.com>
2021-03-31 18:23:37 -0500

    stars.js and stars.pug added

--- routes/table.js
+++ routes/table.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('table', { title: 'Search Results Table' });
+  res.render('table', { title: 'Search Results table' });
 });
 
 module.exports = router;
