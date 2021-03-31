ac7244d3862135eb308c173f3c65a4235dc5ecdd
GUNDAANUTEJ <77635770+GUNDAANUTEJ@users.noreply.github.com>
2021-03-31 15:57:03 -0500

    added table

--- routes/table.js
+++ routes/table.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('table', { title: 'Search results table' });
+  res.render('table', { title: 'Search Results Table' });
 });
 
 module.exports = router;
